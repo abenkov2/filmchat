db971c62a24a5ac03439117d5341dc09687f85e9
abenkov2 <akbenkov79@gmail.com>
2025-06-18 09:14:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const users = [
+  {
+    userMessage: 'Hi',
+    userName: 'Char One',
+  },
+  {
+    userMessage: 'Hello',
+    userName: 'Char Two',
+  },
+];
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    data: users,
+  });
+});
+
+app.post('/', (req, res) => {
+  const inputUserName = req.body.userName;
+  const inputUserMessage = req.body.userMessage;
+
+  users.push({
+    userName: inputUserName,
+    userMessage: inputUserMessage,
+  });
+
+  res.render('home', {
+    data: users,
+  });
+});
+
+app.listen(3000, (req, res) => {
+  console.log('App is running on port 3000');
+});
